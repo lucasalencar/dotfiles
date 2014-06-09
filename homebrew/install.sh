e4895c824f6640ddf3d492b2409d0b825ad23ea5
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2014-06-09 12:23:58 -0300

    Add casks to install apps automatically.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,11 +8,24 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
+  echo "  Installing Homebrew."
   ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
 fi
 
 # Install homebrew packages
 # brew install grc coreutils spark
+
+# Install brew cask to have apps out of the box
+if test $(which brew) then
+  echo "  Installing brew cask and apps."
+  brew tap caskroom/cask > /tmp/tap-cask.log
+  brew tap caskroom/versions > /tmp/tap-cask-versions.log
+  brew install brew-cask > /tmp/brew-cask-install.log
+
+  while read app; do
+    brew cask install $app
+  done < apps
+  unset app
+fi
 
 exit 0
