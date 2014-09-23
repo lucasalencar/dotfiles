d7c7c469e2b57931d4cac106e3213829c1662331
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2014-09-23 12:43:31 -0300

    Log for zsh installation and new plugins.

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,11 +1,11 @@
 # !/bin/sh
-# 
+#
 # Install zsh and oh-my-zsh
 
 if test ! $(which zsh)
 then
   echo "  Installing zsh and oh-my-zsh."
-  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh > /tmp/zsh-install.log
 fi
 
 exit 0
