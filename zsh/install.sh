f354069f5301972e40f9a3a3f159274442e5a418
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2014-10-17 04:05:35 -0300

    OhMyZsh installation check fix.

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,7 +2,7 @@
 #
 # Install zsh and oh-my-zsh
 
-if test ! $(which zsh)
+if test ! $(which ohmyzsh)
 then
   echo "  Installing zsh and oh-my-zsh."
   curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh > /tmp/zsh-install.log
