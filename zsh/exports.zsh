817264fd3b5ed0f5e96f17d65a349bafc141aa0d
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2015-02-24 15:46:27 -0300

    Add automation scripts to dotfiles.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,5 +3,8 @@ export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin
 # Command lines tools from Postgres.app
 export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
 
+# Adds to path automation scripts
+export PATH=$PATH:$DOTFILES_ROOT/automations
+
 # Default editor
 export EDITOR="mate -w"
