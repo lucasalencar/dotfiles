1636866d6b4c385f09210ba3bf8a331237562a2a
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2015-09-02 12:15:18 -0300

    Update log schema for homebrew install.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,21 +5,24 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+LOG="/tmp/install-homebrew.log"
+
 # Check for Homebrew
 if test ! $(which brew); then
   echo "  Installing Homebrew."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > $LOG
 fi
 
 # Install homebrew packages
-# brew install grc coreutils spark
+#brew install grc coreutils spark
+brew install coreutils
 
 # Install brew cask to have apps out of the box
 if test $(which brew); then
-  echo "  Installing brew cask and apps."
-  brew tap caskroom/cask > /tmp/tap-cask.log
-  brew tap caskroom/versions > /tmp/tap-cask-versions.log
-  brew install brew-cask > /tmp/brew-cask-install.log
+  echo "  Installing brew-cask and GUI apps."
+  brew tap caskroom/cask >> $LOG
+  brew tap caskroom/versions >> $LOG
+  brew install brew-cask >> $LOG
 
   while read app; do
     brew cask install $app
