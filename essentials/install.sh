4b2ff6b8d7b9bf751f3fa74ce24175a9b9b605ec
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2015-09-28 23:10:04 -0300

    Installation for smyck color theme.

--- essentials/install.sh
+++ essentials/install.sh
@@ -7,4 +7,12 @@ curl -o /tmp/PlayButtoniTunesPatch.zip http://www.thebitguru.com/site_media/uplo
 unzip -d /tmp /tmp/PlayButtoniTunesPatch.zip
 sudo bash /tmp/PlayButtoniTunesPatch/Patch.command
 
+# Installing Smyck color scheme
+git clone https://github.com/hukl/Smyck-Color-Scheme.git /tmp/smyck
+open /tmp/smyck/Smyck.terminal
+
+# Copy smyck vim theme to janus
+mkdir -p ~/.janus/vim/colors/smyck/colors/
+cp /tmp/smyck/smyck.vim ~/.janus/vim/colors/smyck/colors/smyck.vim
+
 exit 0
