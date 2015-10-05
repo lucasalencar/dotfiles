2d878a314b224f21485b59b78960365084fe12f2
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2015-10-05 01:08:29 -0300

    Fix installation of smyck theme.

--- vim/install.sh
+++ vim/install.sh
@@ -12,15 +12,22 @@ then
 
   # Installing additional plugins
   EXTRAS="$HOME/.janus"
-  mkdir EXTRAS
+  mkdir -p $EXTRAS
 
   git clone https://github.com/jistr/vim-nerdtree-tabs.git $EXTRAS/vim-nerdtree-tabs >> $LOG
   git clone https://github.com/airblade/vim-gitgutter.git $EXTRAS/vim-gitgutter >> $LOG
   git clone https://github.com/bling/vim-airline.git $EXTRAS/vim-airline >> $LOG
 
+  # Add powerline fonts
   git clone https://github.com/powerline/fonts.git $EXTRAS/powerline-fonts >> $LOG
   bash $EXTRAS/powerline-fonts/install.sh
   rm -rf $EXTRAS/powerline-fonts
+
+  # Add Smych theme
+  git clone https://github.com/hukl/Smyck-Color-Scheme.git $EXTRAS/Smyck-Color-Scheme >> $LOG
+  mkdir -p $EXTRAS/smyck/colors/
+  cp $EXTRAS/Smyck-Color-Scheme/smyck.vim $EXTRAS/smyck/colors/smyck.vim
+  rm -rf $EXTRAS/Smyck-Color-Scheme
 fi
 
 exit 0
