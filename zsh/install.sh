a6d3d9293da53ff582b08473b31565716ad65f12
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2015-10-05 01:50:02 -0300

    Add zsh-completions for better cmd completions.

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,6 +6,9 @@ if test ! $(which ohmyzsh)
 then
   echo "  Installing zsh and oh-my-zsh."
   curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh > /tmp/zsh-install.log
+
+  # Install better zsh-completions
+  git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
 fi
 
 exit 0
