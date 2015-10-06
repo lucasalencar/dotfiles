1f226ed67c2dc31a28bc85e98962741833284036
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2015-10-05 22:17:22 -0300

    Fix Postgres.app path.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,7 @@
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
 
 # Command lines tools from Postgres.app
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
+export PATH=$PATH:$HOME/Applications/Postgres.app/Contents/Versions/9.4/bin
 
 # Adds to path automation scripts
 export PATH=$PATH:$DOTFILES_ROOT/automations
