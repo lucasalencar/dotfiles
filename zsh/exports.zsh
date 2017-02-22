0c41d81962c7ccc820c21d6326d54dd09ef295a9
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2017-02-22 09:00:07 -0300

    Go back to using anaconda as my python distribution

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -17,4 +17,5 @@ export EDITOR="mvim"
 # Uses anaconda first
 # Just because this fucking shit of python does not work properly
 # I have to use this workaround to use Jupyter correctly.
-export PATH=/Users/lucas/anaconda/bin:$PATH
+# export PATH=/Users/lucas/anaconda/bin:$PATH
+export PATH=$HOME/anaconda3/bin:"$PATH"
