288d89b8593702aaefdcc2f175c5d66db24754f8
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-03-15 12:10:18 -0300

    Add clojure linter to profiles

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[venantius/ultra "0.5.2"]]
+{:user {:plugins [[venantius/ultra "0.5.2"]
+                  [jonase/eastwood "0.2.5"]]
         :dependencies [[pjstadig/humane-test-output "0.8.3"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
