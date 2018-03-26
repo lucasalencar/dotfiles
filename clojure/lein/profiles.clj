c98b82ddece5abd8ddfb0149ad16a1171d685fb6
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-03-26 09:36:27 -0300

    Add exclusions to clojure eastwood linter

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[venantius/ultra "0.5.2"]
-                  [jonase/eastwood "0.2.5"]]
+                  [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]]
         :dependencies [[pjstadig/humane-test-output "0.8.3"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
