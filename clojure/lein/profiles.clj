71e8cfe0d9a357a359e748f627684a11f6f8ab9e
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-04-21 18:16:36 -0300

    Add vim-fireplace to vimrc
    The plugin helps code in Clojure with nice mappings and connecting to a
    repl instance.

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[venantius/ultra "0.5.2"]
-                  [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]]
+                  [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
+                  [cider/cider-nrepl "0.16.0"]]
         :dependencies [[pjstadig/humane-test-output "0.8.3"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
