3d16ed9721b191e18870095f8498f68006b6b70e
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-04-25 11:06:46 -0300

    Remove path for Postgres.app bins

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,9 +1,6 @@
 # Prefer homebrew installations
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
 
-# Command lines tools from Postgres.app
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
-
 # Adds to path automation scripts
 export PATH=$PATH:$DOTFILES_ROOT/automations
 
