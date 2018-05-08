0b6b4777f52c43bb4f91139bb3daba418040e6ef
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-05-08 14:54:22 -0300

    Add checks to clojure code
    Uses lein eastwood and kibit to check clojure code automatically when
    saving or loading it.

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user {:plugins [[venantius/ultra "0.5.2"]
                   [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
                   [cider/cider-nrepl "0.16.0"]]
-        :dependencies [[pjstadig/humane-test-output "0.8.3"]]
+        :dependencies [[pjstadig/humane-test-output "0.8.3"]
+                       [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+                       [jonase/kibit "0.1.3" :exclusions [org.clojure/clojure]]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
