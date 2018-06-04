a411990e4f0b272ac3910caa6c30c139bc6a9414
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-06-04 15:01:46 -0300

    Add code linter and formatters to lein profile

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[venantius/ultra "0.5.2"]
                   [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
+                  [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]
+                  [slamhound "1.5.5"]
                   [cider/cider-nrepl "0.16.0"]]
         :dependencies [[pjstadig/humane-test-output "0.8.3"]
                        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
