6c7204e537659ec250c847f6f7757aca3e4c0ad3
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-02-04 21:16:06 -0200

    Add functions to manipulate windows on tmux

--- zsh/funcs.zsh
+++ zsh/funcs.zsh
@@ -12,3 +12,30 @@ vf () {
   FILE=$(fzf)
   nvim $FILE
 }
+
+fzf_tmux_window_index () {
+  selected_window=$(tmux list-windows -F "#I: #{pane_current_path}" | fzf-tmux)
+  WINDOW_INDEX=$(echo "$selected_window" | cut -d ":" -f 1)
+  echo $WINDOW_INDEX
+  return $WINDOW_INDEX
+}
+
+fzf_tmux_select_window () {
+  fzf_tmux_window_index | xargs tmux select-window -t
+}
+
+tmux_current_window_index () {
+  current_index=$(tmux display-message -p '#I')
+  echo $current_index
+  return $current_index
+}
+
+fzf_tmux_swap_window_closer () {
+  current_window=$(tmux_current_window_index)
+  incremented=$((current_window + 1))
+
+  selected_window=$(fzf_tmux_window_index)
+
+  tmux swap-window -s $selected_window -t $incremented
+  tmux move-window -r # Renumbers all windows according to order
+}
