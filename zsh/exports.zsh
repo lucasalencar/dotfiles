23dea11633ef5f7d604669addab4f891467d97be
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-02-04 21:51:05 -0200

    Add fzf-tmux functions to scripts directory

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,11 +2,11 @@
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
 
 # Add Go Paths
-export GOPATH=$HOME/code/go
-export PATH=$PATH:$GOPATH/bin
+#export GOPATH=$HOME/code/go
+#export PATH=$PATH:$GOPATH/bin
 
 # Default editor
 export EDITOR="nvim"
 
 # Uses anaconda first for python
-export PATH=/usr/local/anaconda3/bin:"$PATH"
+export PATH=$HOME/.dotfiles/scripts:/usr/local/anaconda3/bin:"$PATH"
