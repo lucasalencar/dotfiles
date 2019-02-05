a627352b475b78162b7e88f9e8f57da57fdb40f6
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-02-04 22:07:24 -0200

    Move some functions into scripts
    Only ncd stays as function, because changing directory is not possible
    through scripts, only functions.

--- zsh/funcs.zsh
+++ zsh/funcs.zsh
@@ -2,13 +2,3 @@ ncd () {
   PROJECT=$(ls $NU_HOME | fzf)
   cd "$NU_HOME/$PROJECT"
 }
-
-fgb () {
-  BRANCH=$(git branch | cut -c 3- | fzf)
-  git checkout $BRANCH
-}
-
-vf () {
-  FILE=$(fzf)
-  nvim $FILE
-}
