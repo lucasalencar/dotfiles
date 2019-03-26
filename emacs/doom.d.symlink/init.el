134176fb4a1b43cf780a6866ca93226e6b57d968
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-03-26 20:06:37 -0300

    Add ivy fuzzy config to emacs

--- emacs/doom.d.symlink/init.el
+++ emacs/doom.d.symlink/init.el
@@ -15,7 +15,8 @@
        company           ; the ultimate code completion backend
        ;;helm             ; the *other* search engine for love and life
        ;;ido              ; the other *other* search engine...
-       ivy              ; a search engine for love and life
+       (ivy               ; a search engine for love and life
+         +fuzzy)
 
        :ui
        ;;deft              ; notational velocity for Emacs
