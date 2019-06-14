f950d2a5e123e9aec9a2965472235533347a0711
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-06-14 10:10:43 -0300

    Add some new workspaces bindings to switch between them

--- emacs/doom.d.symlink/config.el
+++ emacs/doom.d.symlink/config.el
@@ -47,5 +47,16 @@
 ;; Workspaces
 
 (map! (:when (featurep! :ui workspaces)
-        :n "`n" #'+workspace/switch-right
-        :n "`p" #'+workspace/switch-left))
+        :n "`n"  #'+workspace/switch-right
+        :n "`p"  #'+workspace/switch-left
+        :n "`x"  #'+workspace/close-window-or-workspace
+        :g "s-1" #'+workspace/switch-to-0
+        :g "s-2" #'+workspace/switch-to-1
+        :g "s-3" #'+workspace/switch-to-2
+        :g "s-4" #'+workspace/switch-to-3
+        :g "s-5" #'+workspace/switch-to-4
+        :g "s-6" #'+workspace/switch-to-5
+        :g "s-7" #'+workspace/switch-to-6
+        :g "s-8" #'+workspace/switch-to-7
+        :g "s-9" #'+workspace/switch-to-8
+        :g "s-0" #'+workspace/switch-to-final))
