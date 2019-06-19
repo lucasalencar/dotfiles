41015a969e9b0095ae2460513f0dcdae5b1fd800
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-06-19 14:12:15 -0300

    Align keyword

--- emacs/doom.d.symlink/init.el
+++ emacs/doom.d.symlink/init.el
@@ -17,7 +17,7 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy               ; a search engine for love and life
-         +fuzzy)
+        +fuzzy)
 
        :ui
        ;;deft              ; notational velocity for Emacs
