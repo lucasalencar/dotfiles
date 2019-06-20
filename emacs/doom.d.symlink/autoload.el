4e17f7cb5c4804de534c927e4f67ae1dc24b1bcf
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-06-20 14:49:25 -0300

    Change custom clean-ns fn name

--- emacs/doom.d.symlink/autoload.el
+++ emacs/doom.d.symlink/autoload.el
@@ -1,9 +1,10 @@
 ;;; ~/.dotfiles/emacs/doom.d.symlink/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun user/fix-ns ()
-  "(WIP) Fix ns breaklines."
+(defun user/clean-ns ()
+  "(WIP) Clean ns but removing breaklines when done by refactor-nrepl."
   (interactive)
+  ;; (cljr-clean-ns)
   (goto-char (point-min))
   (while (re-search-forward "\n\s*:as\n\s*" nil t)
     (save-match-data
