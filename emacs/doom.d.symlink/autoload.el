840ca451db153c786ee5187ca7eeb1908c93b05d
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-07-16 18:40:38 -0300

    Reenable clean ns fn

--- emacs/doom.d.symlink/autoload.el
+++ emacs/doom.d.symlink/autoload.el
@@ -4,7 +4,7 @@
 (defun user/clean-ns ()
   "(WIP) Clean ns but removing breaklines when done by refactor-nrepl."
   (interactive)
-  ;; (cljr-clean-ns)
+  (cljr-clean-ns)
   (goto-char (point-min))
   (while (re-search-forward "\n\s*:as\n\s*" nil t)
     (save-match-data
