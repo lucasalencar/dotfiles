9b5c85214d19db8f4ea12fa7d72b30c0def56055
Lucas Alencar <alencar.lucas.a@gmail.com>
2021-03-20 12:06:49 -0300

    Update lein profile versions

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,8 +1,8 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.21.1"]
-            [lein-ancient "0.6.15"]
-            [refactor-nrepl "2.4.0"]
-            [nrepl "0.6.0"]]
+ {:plugins [[cider/cider-nrepl "0.25.9"]
+            [lein-ancient "0.7.0"]
+            [refactor-nrepl "2.5.1"]
+            [nrepl "0.8.3"]]
 
   :dependencies [[clj-kondo "2019.05.26-alpha"]]
 
