0df616bf26ff663c9e245974975090b0803417e5
Lucas Alencar <alencar.lucas.a@gmail.com>
2023-04-26 11:57:56 -0300

    Rename dom symlink folder

---
+++ emacs/doom.symlink/local.el
@@ -0,0 +1,6 @@
+;;; ~/.dotfiles/emacs/doom.d.symlink/local.el -*- lexical-binding: t; -*-
+
+(let ((nudev-emacs-path "~/dev/nu/nudev/ides/emacs/"))
+  (when (file-directory-p nudev-emacs-path)
+    (add-to-list 'load-path nudev-emacs-path)
+    (require 'nu)))
