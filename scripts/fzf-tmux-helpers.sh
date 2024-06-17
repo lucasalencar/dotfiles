c3b9739ea246a605f8b20412481f235599465558
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-06-17 15:28:57 -0300

    Fix fzf tmux commands to select window

--- scripts/fzf-tmux-helpers.sh
+++ scripts/fzf-tmux-helpers.sh
@@ -3,8 +3,7 @@
 fzf_tmux_window_index () {
   selected_window=$(tmux list-windows -F "#I: #{pane_current_path}" | fzf-tmux)
   WINDOW_INDEX=$(echo "$selected_window" | cut -d ":" -f 1)
-  echo $WINDOW_INDEX
-  return $WINDOW_INDEX
+  echo "$WINDOW_INDEX"
 }
 
 tmux_current_window_index () {
