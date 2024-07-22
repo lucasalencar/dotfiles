ba5eb4d9cfa56c4ade6995ee92a826a1c17afc02
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-07-22 16:04:45 -0300

    Remove colors from ccd

--- scripts/ccd.sh
+++ scripts/ccd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ccd () {
-  if [ ! $CODE_HOME ]
+  if [ ! "$CODE_HOME" ]
   then
     CODE_HOME="$HOME"
   fi
 
-  project=$(ls $CODE_HOME | fzf)
-  if [ $project ]
+  project=$(ls --color=never "$CODE_HOME" | fzf)
+  if [ "$project" ]
   then
-    cd "$CODE_HOME/$project"
+    cd "$CODE_HOME/$project" || return
   fi
 }
