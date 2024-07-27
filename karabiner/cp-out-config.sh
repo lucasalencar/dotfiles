1b924ffd9bf28afe8b416fd164a280d46a514ccf
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-07-27 12:55:43 -0300

    Use current_dir properly

--- karabiner/cp-out-config.sh
+++ karabiner/cp-out-config.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 source helpers
 
-current_dir # Sets $CURRENT_DIR variable
 mkdir -p "$HOME/.config/karabiner/"
-cp "$CURRENT_DIR/karabiner.json.symlink" "$HOME/.config/karabiner/karabiner.json"
+cp "$(current_dir)/karabiner.json.symlink" "$HOME/.config/karabiner/karabiner.json"
 
 exit 0
