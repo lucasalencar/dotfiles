d3068740b22c36e150ebc985384e8604d582dc08
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-08-02 19:19:20 -0300

    Remove assets from karabiner

--- karabiner/cp-out-config.sh
+++ karabiner/cp-out-config.sh
@@ -3,6 +3,5 @@ source helpers
 
 mkdir -p "$HOME/.config/karabiner/"
 cp "$(current_dir)/karabiner.json.symlink" "$HOME/.config/karabiner/karabiner.json"
-cp "$(current_dir)/assets/complex_modifications/"*.json "$HOME/.config/karabiner/assets/complex_modifications/"
 
 exit 0
