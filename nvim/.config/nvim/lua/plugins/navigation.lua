baa6b0578f806eaf720c6beaf7561793ff0dfd96
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-08-12 18:04:57 -0300

    Add 2 fuzzy finders, because I was not able to decide which to use

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -1,5 +1,26 @@
 return {
   {
+    -- Fuzzy finder
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = "Find files" })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Grep files" })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find for help (manual)" })
+    end
+  },
+  {
+    -- Fuzzy finder
+    'junegunn/fzf.vim',
+    dependencies = { 'junegunn/fzf' },
+    config = function()
+      vim.keymap.set('n', '<C-p>', '<cmd>Files<CR>', { desc = "Find files" })
+    end
+  },
+  {
     -- File manager that works like a normal buffer
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
