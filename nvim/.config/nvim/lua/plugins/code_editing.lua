8a5afc4041d8c10aa4e1a46335638b400398a039
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-08-15 18:29:24 -0300

    Add copilot plugin

--- nvim/.config/nvim/lua/plugins/code_editing.lua
+++ nvim/.config/nvim/lua/plugins/code_editing.lua
@@ -3,5 +3,10 @@ return {
     -- Close brackets, quotes, etc. automatically
     'jiangmiao/auto-pairs',
     event = 'InsertEnter',
+  },
+  {
+    -- Add IA copilot to replace my job
+    'github/copilot.vim',
+    event = 'InsertEnter',
   }
 }
