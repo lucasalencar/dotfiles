deba40f95510773ec33d1bad064744d017e5c6ef
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-08-15 19:22:03 -0300

    Add some more code editing plugins

--- nvim/.config/nvim/lua/plugins/code_editing.lua
+++ nvim/.config/nvim/lua/plugins/code_editing.lua
@@ -8,5 +8,17 @@ return {
     -- Add IA copilot to replace my job
     'github/copilot.vim',
     event = 'InsertEnter',
-  }
+  },
+  {
+    -- Surround elements with brackets, quotes, etc.
+    'tpope/vim-surround',
+  },
+  {
+    -- Opens repeat command (`.`) to plugins
+    'tpope/vim-repeat',
+  },
+  {
+    -- Switch between representations of similar data types (e.g. switch array for a set)
+    'AndrewRadev/switch.vim',
+  },
 }
