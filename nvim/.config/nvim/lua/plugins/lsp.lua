4d61e7db92965c3ac99701658f4529a7fce54708
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-08-16 15:36:05 -0300

    Add handlers to mason_lspconfig setup

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -23,22 +23,22 @@ return {
 
           -- Automatically install missing LSPs based on ensure_installed
           automatic_installation = true,
+
+          -- Specify how to setup specific LSPs (place to put custom configurations)
+          handlers = {
+            -- The first entry (without a key) will be the default handler
+            -- and will be called for each installed server that doesn't have
+            -- a dedicated handler.
+            function (server_name) -- default handler (optional)
+              lspconfig[server_name].setup {}
+            end,
+            -- Next, you can provide a dedicated handler for specific servers.
+            -- For example, a handler override for the `rust_analyzer`:
+            -- ["rust_analyzer"] = function ()
+              --     require("rust-tools").setup {}
+            -- end
+          }
         })
-
-
-      mason_lspconfig.setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-          lspconfig[server_name].setup {}
-        end,
-        -- Next, you can provide a dedicated handler for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-        -- ["rust_analyzer"] = function ()
-          --     require("rust-tools").setup {}
-        -- end
-      }
     end
   },
 }
