2ffeff046dda26aae2311ff5ad16ff33907fd970
Lucas Alencar <alencar.lucas.a@gmail.com>
2024-08-16 15:33:29 -0300

    Create local references for libs

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -7,11 +7,15 @@ return {
       "williamboman/mason-lspconfig.nvim" -- Extension to integrate mason.nvim better with nvim-lspconfig
     },
     config = function()
-      require("mason").setup()
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+
+      mason.setup()
 
       vim.keymap.set("n", "<leader>ll", "<cmd>Mason<CR>", { desc = "Mason LSP manager" })
 
-      require("mason-lspconfig").setup({
+      mason_lspconfig.setup({
           -- LSPs list for installation
           ensure_installed = {
             "clojure_lsp"
@@ -22,12 +26,12 @@ return {
         })
 
 
-      require("mason-lspconfig").setup_handlers {
+      mason_lspconfig.setup_handlers {
         -- The first entry (without a key) will be the default handler
         -- and will be called for each installed server that doesn't have
         -- a dedicated handler.
         function (server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {}
+          lspconfig[server_name].setup {}
         end,
         -- Next, you can provide a dedicated handler for specific servers.
         -- For example, a handler override for the `rust_analyzer`:
