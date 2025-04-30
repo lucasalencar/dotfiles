8ead97b32eda5af728effeedfc5eacfa7c65e86a
Lucas Alencar <alencar.lucas.a@gmail.com>
2025-04-30 19:28:17 -0300

    Add cursor installation to install script

--- cursor/install.sh
+++ cursor/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+brew install cursor
+
 cursor --install-extension vscodevim.vim # Vim
 cursor --install-extension danprince.vsnetrw # oil.nvim / netrw / vim-vinegar
 cursor --install-extension chaitanyashahare.lazygit # Better git
