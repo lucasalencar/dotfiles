e33bf4c4235f3b5a163e1946488e8b2be065f236
Lucas Alencar <alencar.lucas.a@gmail.com>
2025-04-30 19:25:38 -0300

    Add installation and update script

---
+++ cursor/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cursor --install-extension vscodevim.vim # Vim
+cursor --install-extension danprince.vsnetrw # oil.nvim / netrw / vim-vinegar
+cursor --install-extension chaitanyashahare.lazygit # Better git
+
+# Clojure extensions
+cursor --install-extension betterthantomorrow.calva
+cursor --install-extension betterthantomorrow.calva-spritz
