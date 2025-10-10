92ca6ef4ce6062b489e34b07080fb31dcc290bd9
Lucas Alencar <alencar.lucas.a@gmail.com>
2025-10-10 17:12:10 -0300

    Only set CODE_HOME when it is not set

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -9,4 +9,5 @@ export EDITOR="nvim"
 
 export LANG="en_US.UTF-8"
 
-export CODE_HOME="$HOME/code/"
+# Respect an existing CODE_HOME if already set (e.g., by the launching shell)
+export CODE_HOME="${CODE_HOME:-$HOME/code/}"
