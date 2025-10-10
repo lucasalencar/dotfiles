457da04f29f83d5bdac5cba2a3a4b98203968cc8
Lucas Alencar <alencar.lucas.a@gmail.com>
2025-10-10 17:18:20 -0300

    Add resolver for code home folder variable

--- scripts/ccd.sh
+++ scripts/ccd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ccd () {
-  if [ ! "$CODE_HOME" ]
-  then
-    CODE_HOME="$HOME"
-  fi
+source "$DOTFILES_ROOT/scripts/fzf-tmux-helpers.sh"
 
-  project=$(ls --color=never "$CODE_HOME" | fzf)
+ccd () {
+  local base_dir
+  base_dir=$(resolve_code_home)
+
+  project=$(ls --color=never "$base_dir" | fzf)
   if [ "$project" ]
   then
-    cd "$CODE_HOME/$project" || return
+    cd "$base_dir/$project" || return
   fi
 }
--- scripts/fzf-tmux-helpers.sh
+++ scripts/fzf-tmux-helpers.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+resolve_code_home () {
+  local base_dir
+  base_dir="${CODE_HOME:-$HOME/code}"
+  # Expand ~ if present
+  base_dir=$(eval echo "$base_dir")
+  echo "$base_dir"
+}
+
 fzf_tmux_window_index () {
   selected_window=$(tmux list-windows -F "#I: #{pane_current_path}" | fzf-tmux)
   WINDOW_INDEX=$(echo "$selected_window" | cut -d ":" -f 1)
@@ -12,5 +20,7 @@ tmux_current_window_index () {
 }
 
 fzf_tmux_code_dir () {
-  ls "$CODE_HOME" | fzf-tmux
+  local base_dir
+  base_dir=$(resolve_code_home)
+  ls "$base_dir" | fzf-tmux
 }
